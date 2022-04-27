e66335283a8852f7c5b87b86458269fb7952b5a7
vennymeida <vnymeida@gmail.com>
2022-04-27 22:08:38 +0700

    Logout Praktikum1 JS11

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -53,4 +53,17 @@ class AuthController extends Controller
             'user' => $user,
         ]);
     }
+
+    public function logout()
+    {
+        try {
+            auth()->user()->tokens()->delete();
+            return $this->apiSuccess('Tokens revoked');
+        } catch (\Throwable $e) {
+            throw new HttpResponseException($this->apiError(
+                null,
+                Response::HTTP_INTERNAL_SERVER_ERROR,
+            ));
+        }
+    }
 }
